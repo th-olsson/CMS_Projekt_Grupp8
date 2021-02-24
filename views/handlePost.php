72e812c46aafb72fa1271903b8ad2a5a0d35b9af
Thomas Olsson <olss.thom@gmail.com>
2021-02-24 20:57:07 +0100

    minor fix of indentation and comment

--- views/handlePost.php
+++ views/handlePost.php
@@ -1,5 +1,5 @@
 <?php
 
-    print_r($_POST);
+    print_r($_POST); //Testing data 
 
 ?>
--- views/post.php
+++ views/post.php
@@ -16,26 +16,26 @@ $date = date('Y-m-d'); //date('Y-m-d') returns current date in yyyy-mm-dd format
 <body>
 <h1>Create a post</h1>
 
-    <!-- Form to send data of 'Date', 'Writer', 'Category', 'Description' 'Image URL', 'Title' and 'Content'. -->
-    <form action="handlePost.php" method="post">
-        <input type="text" name="date" readonly value="<?=$date?>">
-        <select name="writer">
-            <option value="admin1">admin1</option>
-            <option value="admin2">admin2</option>
-            <option value="admin3">admin3</option>
-        </select>
-        <label for="category">Category</label>
-        <input type="text" name="category" placeholder="Category of blog post"></input>
-        <label for="description">Description</label>
-        <input type="text" name="description" placeholder="Describe blog post"></input>
-        <label for="image">Image URL</label>
-        <input type="text" name="image" placeholder="Paste image URL here"></input>
-        <label for="title">Title</label>
-        <input type="text" name="title" placeholder="Title of blog post"></input>
-        <label for="content">Content</label>
-        <textarea name="content" id="" cols="30" rows="10" placeholder="Content of blog post.."></textarea>
-        <input type="submit" value="Submit"></input>
-    </form>
+<!-- Form to send data of 'Date', 'Writer', 'Category', 'Description' 'Image URL', 'Title' and 'Content'. -->
+<form action="handlePost.php" method="post">
+    <input type="text" name="date" readonly value="<?=$date?>">
+    <select name="writer">
+        <option value="admin1">admin1</option>
+        <option value="admin2">admin2</option>
+        <option value="admin3">admin3</option>  
+    </select>
+    <label for="category">Category</label>
+    <input type="text" name="category" placeholder="Category of blog post"></input>
+    <label for="description">Description</label>
+    <input type="text" name="description" placeholder="Describe blog post"></input>
+    <label for="image">Image URL</label>
+    <input type="text" name="image" placeholder="Paste image URL here"></input>
+    <label for="title">Title</label>
+    <input type="text" name="title" placeholder="Title of blog post"></input>
+    <label for="content">Content</label>
+    <textarea name="content" id="" cols="30" rows="10" placeholder="Content of blog post.."></textarea>
+    <input type="submit" value="Submit"></input>
+</form>
 
 </body>
 </html>
