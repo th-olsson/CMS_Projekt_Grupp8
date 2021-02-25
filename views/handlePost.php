3f497d491ca6b6215afd80aee6c82e80c5e55758
Thomas Olsson <olss.thom@gmail.com>
2021-02-25 12:05:38 +0100

    completes basic template to send data from post.php to 'posts'-table in database

--- views/handlePost.php
+++ views/handlePost.php
@@ -2,4 +2,39 @@
 
     print_r($_POST); //Testing data 
 
+    #Variables
+    $date = $_POST['date'];
+    $author = $_POST['author'];
+    $category = $_POST['category'];
+    $description = $_POST['description'];
+    $image = $_POST['image'];
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+
+    $userId = 2; //Temporary placeholder. Needs to get real userId from $_POST or using $_POST['author'] to fetch from database
+
+    #Database connection
+    $dsn = "mysql:host=localhost;dbname=bloggdb";
+    $user = "root";
+    $password = "";
+    $pdo = new PDO($dsn, $user, $password);
+
+    //SQL
+    $sql = 'INSERT INTO posts (Date, UserID, Category, Description, Image, Title, Content) VALUES (:date_IN, :userId_IN, :category_IN, :description_IN, :image_IN, :title_IN, :content_IN)';
+
+    $stm = $pdo->prepare($sql);
+    $stm->bindParam(':date_IN', $date);
+    $stm->bindParam(':userId_IN', $userId);
+    $stm->bindParam(':category_IN', $category);
+    $stm->bindParam(':description_IN', $description);
+    $stm->bindParam(':image_IN', $image);
+    $stm->bindParam(':title_IN', $title);
+    $stm->bindParam(':content_IN', $content);
+
+    if($stm->execute()){
+        echo "your data has been successfully added to the database";
+    } else {
+        echo "something went wrong";
+    }
+
 ?>
