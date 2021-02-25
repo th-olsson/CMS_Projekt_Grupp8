cec98ee78c2d9fe86eef706680cacaea1f6d093d
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2021-02-25 13:49:35 +0100

    index.php and public includes

---
+++ Includes/head.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="other/Css/style.css">
+	<meta charset="UTF-8">
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("includes/head.php") ?>
+	<title>Blogg | Homepage </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- navbar -->
+		<?php include("includes/header.php") ?>
+		<!-- // navbar -->
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+		<!-- // Page content -->
+
+		<!-- footer -->
+		<?php include("includes/footer.php") ?>
