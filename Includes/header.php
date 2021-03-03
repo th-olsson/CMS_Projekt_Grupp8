2aaa082da77ce5688b2627ebf6231c2b397d5891
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2021-03-03 10:43:29 +0100

    Posts added to mainpage w include

--- Includes/header.php
+++ Includes/header.php
@@ -1,14 +1,34 @@
+<!-- navbar -->
 <header class="navbar">
-	<div class="logo_div">
-		<a href="index.php">
-			<h1>Millhouse</h1>
-		</a>
-	</div>
-	<ul>
-		<li><a class="active" href="index.php">Home</a></li>
-		<li><a href="#about">About Millhouse</a></li>
-		<li><a href="views/login.php">Login</a></li>
-		<li><a href="views/register.php">Register</a></li>
+            <div class="logo_div">
+                <a href="index.php">
+                    <h1>Millhouse</h1>
+                </a>
+            </div>
+            <ul>
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a href="#about">About Millhouse</a></li>
+                <?php
+                if (!isset($_SESSION['is_Login'])) { ?>
 
-	</ul>
-</header>
+                    <li><a href="views/login.php">Login</a></li>
+                <?php  } ?>
+
+                <li><a href="views/register.php">Register</a></li>
+                <?php
+                if (@$_SESSION['role'] == "admin") { ?>
+                    <li><a href="views/post.php">Create new Post</a></li>
+
+                    <li><a href="views/viewPost.php">Edit Post</a></li>
+
+
+                <?php } ?>
+                <?php
+                if (@$_SESSION['role'] == "admin" || @$_SESSION['role'] == "user") { ?>
+
+                    <li><a href="views/logout.php">Logout</a></li>
+
+                <?php  } ?>
+
+            </ul>
+        </header>
--- views/handlePost.php
+++ views/handlePost.php
@@ -4,26 +4,21 @@
 $date = $_POST['date'];
 $author = $_POST['author'];
 $category = $_POST['category'];
-$description = $_POST['description'];
 $image = $_POST['image'];
 $title = $_POST['title'];
 $content = $_POST['content'];
 $userId = $_POST['userId'];
 
 #Database connection
-$dsn = "mysql:host=localhost;dbname=bloggdb";
-$user = "root";
-$password = "root";
-$pdo = new PDO($dsn, $user, $password);
+ include ("../includes/database.php");
 
 //SQL - insert data from $_POST to posts-table
-$sql = 'INSERT INTO posts (Date, UserID, Category, Description, Image, Title, Content) VALUES (:date_IN, :userId_IN, :category_IN, :description_IN, :image_IN, :title_IN, :content_IN)';
+$sql = 'INSERT INTO posts (Date, UserID, Category,  Image, Title, Content) VALUES (:date_IN, :userId_IN, :category_IN, :image_IN, :title_IN, :content_IN)';
 
-$stm = $pdo->prepare($sql);
+$stm = $db->prepare($sql);
 $stm->bindParam(':date_IN', $date);
 $stm->bindParam(':userId_IN', $userId);
 $stm->bindParam(':category_IN', $category);
-$stm->bindParam(':description_IN', $description);
 $stm->bindParam(':image_IN', $image);
 $stm->bindParam(':title_IN', $title);
 $stm->bindParam(':content_IN', $content);
