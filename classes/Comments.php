987c2d9e5276dd3fc809320e857e66040eb61be7
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-03-12 09:44:43 +0100

    width added to image and post content

--- classes/Comments.php
+++ classes/Comments.php
@@ -1,21 +1,19 @@
 <?php
 
-class Comment {
+class Comment
+{
     private $id;
     private $content;
     private $date;
     private $postId;
     private $userId;
 
-    function __construct($id_IN, $content_IN, $date_IN, $postId_IN, $userId){
+    function __construct($id_IN, $content_IN, $date_IN, $postId_IN, $userId_IN)
+    {
         $this->id = $id_IN;
         $this->content = $content_IN;
         $this->date = $date_IN;
         $this->postId = $postId_IN;
         $this->userId = $userId_IN;
     }
-
 }
-
-?>
-
