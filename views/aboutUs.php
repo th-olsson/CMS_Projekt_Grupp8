febdfb8d5f80fa224c0774e785981981bd66a76a
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-03-12 18:40:58 +0100

    added aboutus.php

---
+++ views/aboutUs.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/style.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1:wght@500&display=swap" rel="stylesheet">
+    <title>About Us</title>
+
+</head>
+
+<body>
+
+    <?php include("../includes/header.php");
+
+    ?>
+
+
+    <main class="bg">
+        <h1 class=title_text>About Us</h1>
+
+        <div>
+            <p>
+                <span>Millhouse</span> is a wholesale company that sells clothing, accessories and small furnishings to fashion and lifestyle stores.Having been in the industry since 2000, we are one of the trusted brand that our customers rely when it comes to quality and affordable products.Our Mission & Vision is to provide best valued products to our customers. We provide top brands to our customers, and enable our brand partners to reach the widest audience along with 14 Days Money Back Guarantee & 365 days Returns.
+            </p>
+            <img src="/CMS_Projekt_Grupp8/Images/ecommerce.jpg" alt="ecommerce_img" class="textwrap">
+            <p> Millhouse have now plan to reach to you with its new onine e-commerce store, a continuously evolving shopping center online.Just like a shopping center,theres is always something fresh and exciting.With our range of options in <em>watches</em>, <em>sunglasses &</em>
+                <em>smaller furnishings</em>, We continue to give high quality service and long standing commitment to our customers.Follow us on our new and latest updates on our <a href="/CMS_Projekt_Grupp8/index.php"><strong>Blog</strong> </a>
+                and connect with us to let us know what would you like to see and share your experiences with us to serve you better.
+
+            </p>
+
+            <p> Millhouse has annual sales of SEK 75 million and 50 employees, mainly within administration, purchasing and inventory management, and we are expanding our team with our new online store to give you a whole new experience of online shopping.</p>
+
+            </p>
+
+        </div>
+        <h2>Words from our customer</h2>
+        <div class=" quote">
+            <blockquote>
+                ⭐⭐⭐⭐⭐
+                <p>They have a very friendly customer service and after sales service, We are very happy with the Millhouse and will continue shopping with them</p>
+                <cite>-Samuel Beckhem</cite>
+            </blockquote>
+        </div>
+        <div class="quote">
+            <blockquote>
+                ⭐⭐⭐⭐⭐
+                <p>The best in class products and very friendly customer service.</p>
+                <cite>-David Zoe</cite>
+            </blockquote>
+        </div>
+        <div class="quote">
+
+            <blockquote>
+                ⭐⭐⭐⭐⭐
+                <p>Love their products and the vibe of the store</p>
+                <cite>-Anders Malmar</cite>
+            </blockquote>
+        </div>
+        <div class="quote">
+
+            <blockquote>
+                ⭐⭐⭐⭐
+                <p>The best among their competitors!</p>
+                <cite>-Emma Winglet</cite>
+            </blockquote>
+        </div>
+
+
+
+    </main>
+    <?php include "../includes/footer.php"; ?>
+</body>
+
+</html>
