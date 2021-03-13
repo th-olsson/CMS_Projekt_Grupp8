d2ddc4c8514d0bf00b0b8a280483e63347465ef1
Thomas Olsson <olss.thom@gmail.com>
2021-03-13 23:41:52 +0100

    moved font from css to headLinks

--- Includes/headLinks.php
+++ Includes/headLinks.php
@@ -3,9 +3,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Lora&family=Montserrat&display=swap" rel="stylesheet">
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Lora&family=Montserrat&display=swap" rel="stylesheet">
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1:wght@500&display=swap" rel="stylesheet">
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
